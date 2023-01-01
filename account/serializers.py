9d198a083ce3bbda73804b353ce661ddfa7d9d57
olawale afuye <walosha@yahoo.com>
2023-01-01 23:31:25 +0100

    added change pass url

--- account/serializers.py
+++ account/serializers.py
@@ -27,3 +27,16 @@ class CreateUserSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+
+
+class PasswordChangeSerializer(serializers.Serializer):
+    current_password = serializers.CharField(
+        style={"input_type": "password"}, required=True)
+    new_password = serializers.CharField(
+        style={"input_type": "password"}, required=True)
+
+    def validate_current_password(self, value):
+        if not self.context['request'].user.check_password(value):
+            raise serializers.ValidationError(
+                {'current_password': 'Does not match'})
+        return value
--- account/views.py
+++ account/views.py
@@ -1,8 +1,7 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, permissions
 from rest_framework.decorators import action
-from .serializers import CustomObtainTokenPairSerializer
-from .serializers import CreateUserSerializer
+from .serializers import CreateUserSerializer, PasswordChangeSerializer, CustomObtainTokenPairSerializer
 from rest_framework.response import Response
 
 
@@ -23,3 +22,16 @@ class AuthViewSets(viewsets.ModelViewSet):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(methods=['POST'],
+            url_path='change_password',
+            permission_classes=[permissions.IsAuthenticated],
+            detail=False, serializer_class=CreateUserSerializer)
+    def change_user_password(self, request, pk=None):
+        serializer = PasswordChangeSerializer(
+            context={'request': request}, data=request.data)
+        # Another way to write is as in Line 17
+        serializer.is_valid(raise_exception=True)
+        request.user.set_password(serializer.validated_data['new_password'])
+        request.user.save()
+        return Response(data='Password successfully changed', status=status.HTTP_200_OK)
